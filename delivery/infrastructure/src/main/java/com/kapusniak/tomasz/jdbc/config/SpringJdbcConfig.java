a7e1d1d49d1187a51749545afaddee990d227744
whitesnakeIT <105644251+whitesnakeIT@users.noreply.github.com>
2023-05-01 22:01:01 +0200

    Jdbc template (#19)
    * added jdbc config, examples + mappers + tests
    
    * application.properties fixing
    app in container need spring.h2.console.settings.web-allow-others=true

---
+++ delivery/infrastructure/src/main/java/com/kapusniak/tomasz/jdbc/config/SpringJdbcConfig.java
@@ -0,0 +1,35 @@
+package com.kapusniak.tomasz.jdbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("jdbc")
+@ComponentScan(basePackages = "com.kapusniak.tomasz.jdbc")
+public class SpringJdbcConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("test")
+                .addScript("classpath:/db/migration/V1__create-schema.sql")
+                .addScript("classpath:/db/migration/V2__create-tables.sql")
+                .addScript("classpath:/db/migration/V3__insert-data.sql")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+
+}
