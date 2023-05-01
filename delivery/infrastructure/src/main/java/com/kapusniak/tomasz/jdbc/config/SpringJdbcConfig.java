7f5f93e3ed01c21623d672cd831982bcc23779bf
whitesnakeit <tomasz.kapusniak.it@gmail.com>
2023-05-01 22:00:11 +0200

    application.properties fixing app in container need spring.h2.console.settings.web-allow-others=true

--- delivery/infrastructure/src/main/java/com/kapusniak/tomasz/jdbc/config/SpringJdbcConfig.java
+++ delivery/infrastructure/src/main/java/com/kapusniak/tomasz/jdbc/config/SpringJdbcConfig.java
@@ -3,6 +3,7 @@ package com.kapusniak.tomasz.jdbc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -10,6 +11,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("jdbc")
 @ComponentScan(basePackages = "com.kapusniak.tomasz.jdbc")
 public class SpringJdbcConfig {
 
