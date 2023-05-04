7f5cd8d7f55aeaf1d4eb5a996eb2a166d2032ba9
whitesnakeit <tomasz.kapusniak.it@gmail.com>
2023-05-04 18:39:28 +0200

    tests for mappers added, tests coverage upgrade

---
+++ delivery/infrastructure/src/test/java/com/kapusniak/tomasz/jdbc/mapper/CustomerRowMapperTest.java
@@ -0,0 +1,53 @@
+package com.kapusniak.tomasz.jdbc.mapper;
+
+import com.kapusniak.tomasz.entity.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ActiveProfiles("jdbc")
+@TestPropertySource("classpath:application-jdbc-test.properties")
+class CustomerRowMapperTest {
+
+    @Mock
+    private ResultSet resultSet;
+
+    @Autowired
+    private CustomerRowMapper customerRowMapper;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        when(resultSet.getLong("CUSTOMER_ID")).thenReturn(1L);
+        when(resultSet.getString("FIRST_NAME")).thenReturn("John");
+        when(resultSet.getString("LAST_NAME")).thenReturn("Doe");
+        when(resultSet.getString("EMAIL")).thenReturn("john.doe@example.com");
+    }
+
+    @Test
+    @DisplayName("should correctly map resultSet for Customer object")
+    void mapRow() throws SQLException {
+
+        // when
+        Customer customer = customerRowMapper.mapRow(resultSet, 1);
+
+        // then
+        assertNotNull(customer);
+        assertEquals(1L, customer.getId());
+        assertEquals("John", customer.getFirstName());
+        assertEquals("Doe", customer.getLastName());
+        assertEquals("john.doe@example.com", customer.getEmail());
+    }
+}
