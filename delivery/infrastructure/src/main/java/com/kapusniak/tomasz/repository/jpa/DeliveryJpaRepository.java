cf40075033b996e80e1945d5b6086db8bd1240ca
whitesnakeit <tomasz.kapusniak.it@gmail.com>
2023-06-11 22:30:14 +0200

    scheduling added, redis added, changes in open-api api error

--- delivery/infrastructure/src/main/java/com/kapusniak/tomasz/repository/jpa/DeliveryJpaRepository.java
+++ delivery/infrastructure/src/main/java/com/kapusniak/tomasz/repository/jpa/DeliveryJpaRepository.java
@@ -1,14 +1,21 @@
 package com.kapusniak.tomasz.repository.jpa;
 
 import com.kapusniak.tomasz.entity.DeliveryEntity;
+import com.kapusniak.tomasz.openapi.model.DeliveryStatus;
 import com.kapusniak.tomasz.repository.UuidRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 @Transactional(readOnly = true)
 public interface DeliveryJpaRepository extends UuidRepository<DeliveryEntity, UUID> {
+
+    List<DeliveryEntity> findAllByDeliveryStatus(DeliveryStatus deliveryStatus);
+
+    List<DeliveryEntity> findAllByDeliveryStatusAndDeliveryTimeBefore(DeliveryStatus deliveryStatus, LocalDateTime deliveryTime);
 
 }
